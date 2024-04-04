1607d5d3b8ff3e0d88f6c22ce338c96a7abdd135
Gue koksheng <guekoksheng@Gues-Mac-mini.local>
2024-04-05 00:03:14 +0800

    test

---
+++ dependency-injection/ConsoleAppmailSender/LogServices/ConsoleLogProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LogServices
+{
+	public class ConsoleLogProvider : ILogProvider
+	{
+		public ConsoleLogProvider()
+		{
+		}
+
+        public void LogError(string msg)
+        {
+            Console.WriteLine($"ERROR:{msg}");
+        }
+
+        public void LogInfo(string msg)
+        {
+            Console.WriteLine($"Info:{msg}");
+        }
+    }
+}
+
---
+++ dependency-injection/ConsoleAppmailSender/MailServices/MailService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ConfigServices;
+using LogServices;
+
+namespace MailServices
+{
+	public class MailService : IMailService
+	{
+        private readonly ILogProvider log;
+        private readonly IConfigService config;
+		public MailService(ILogProvider log, IConfigService config)
+		{
+            this.log = log;
+            this.config = config;
+		}
+
+        public void Send(string title, string to, string body)
+        {
+            log.LogInfo("准备发送邮件");
+            string smtpServer = config.GetValue("SmtpServer");
+            string userName = config.GetValue("UserName");
+            string password = config.GetValue("Password");
+            Console.WriteLine($"邮件服务器地址{smtpServer}, {userName}, {password}");
+            Console.WriteLine($"真发邮件啦！{title}, {to}, {body}");
+            log.LogInfo("邮件发送完成");
+        }
+    }
+}
+
